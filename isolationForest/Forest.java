7889f4ea589e2e73c46544a0a09cb8254c1424e3
Lara-Dundar <dundar.lara@gmail.com>
2024-10-05 11:31:35 +0200

    Hashmap implementation is done (issues while running, needs to be fixed)

--- isolationForest/Forest.java
+++ isolationForest/Forest.java
@@ -1,6 +1,7 @@
 package isolationForest;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 
 public class Forest {
@@ -11,7 +12,7 @@ public class Forest {
     public Forest(int numberOfTrees) {
         this.trees = new ArrayList<>();
         this.numberOfTrees = numberOfTrees;
-        this. tableForCollectiveEvaluation = new HashMap<>();
+        this.tableForCollectiveEvaluation = new HashMap<>();
     }
 
     public void createForest(List<List<String>> data) {
@@ -22,10 +23,17 @@ public class Forest {
             System.out.println(tree.getShortestPaths(tree.allLeafNodes).size());
 
             List<dataPoint> dataPoints= createDataPoints(tree);
-            for(int j=0 ; j<dataPoints.size() ;j++) {
-                dataPoint each_dataPoint = new dataPoint();
-                if(!tableForCollectiveEvaluation.containsKey(each_dataPoint)) {
-                    // Continue here.
+            List<Integer> pathLengths = createPathLengths(tree);
+
+            for(int j=0 ; j<dataPoints.size() ; j++) {
+                dataPoint each_dataPoint = dataPoints.get(j);
+                int each_pathLength = pathLengths.get(j);
+
+                if (tableForCollectiveEvaluation.containsKey(each_dataPoint)) {
+                    int currentPathLength = tableForCollectiveEvaluation.get(each_dataPoint);
+                    tableForCollectiveEvaluation.put(each_dataPoint, currentPathLength + each_pathLength);
+                } else {
+                    tableForCollectiveEvaluation.put(each_dataPoint, each_pathLength);
                 }
             }
             trees.add(tree);
@@ -34,17 +42,34 @@ public class Forest {
 
         public List<dataPoint> createDataPoints(Tree tree) {
         List<dataPoint> dataPointPerTree = new ArrayList<>();
+
         for(int i=0 ; i<tree.dataPathLengths.size() ; i++) {
             List<String> each_data = tree.dataPathLengths.get(i);
             List<Double> each_dataPoint = new ArrayList<>();
 
-            for (String eachString : each_data) {
-                each_dataPoint.add(Double.parseDouble(eachString));
+            for (int j = 0; j < each_data.size() - 1; j++) {
+                each_dataPoint.add(Double.parseDouble(each_data.get(j)));
             }
             dataPoint data_Point = new dataPoint();
             data_Point.data = each_dataPoint;
             dataPointPerTree.add(data_Point);
         }
         return dataPointPerTree;
+    }
+
+    public List<Integer> createPathLengths(Tree tree) {
+        List<Integer> pathLengths = new ArrayList<>();
+
+        for(int i=0 ; i<tree.dataPathLengths.size() ; i++) {
+            List<String> each_data = tree.dataPathLengths.get(i);
+            pathLengths.add(Integer.parseInt(each_data.get(each_data.size()-1)));
+        }
+        return pathLengths;
+    }
+
+    public void printHashMap() {
+        for (Map.Entry<dataPoint, Integer> entry : tableForCollectiveEvaluation.entrySet()) {
+            System.out.println("DataPoint: " + entry.getKey().data + " -> Path Length: " + entry.getValue());
+        }
     }
 }
--- isolationForest/isolationForest.java
+++ isolationForest/isolationForest.java
@@ -26,6 +26,7 @@ public class isolationForest {
 
         Forest forest = new Forest(10);
         forest.createForest(data);
+        forest.printHashMap();
 
     }
 }
